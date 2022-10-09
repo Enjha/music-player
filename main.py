555d353b5a547c5030c23ce0f568ea63fd36b222
thoma <thomas.ninci@gmail.com>
2022-10-09 19:07:38 +0200

    interface

--- main.py
+++ main.py
@@ -1,51 +1,22 @@
-from ast import main, pattern
+from ast import main
 from fileinput import filename
-from importlib.resources import path
-from multiprocessing.resource_sharer import stop
-from struct import pack
 from tkinter import *
-import tkinter as tk
 import os
 import fnmatch
+from tkinter import filedialog
 import pygame
 from pygame import mixer
-import time
-#import vlc
-from playsound import playsound
-from pydub import AudioSegment
-from pydub.playback import play
 
-root = Tk()
 
-root.title('GeeksforGeeks sound player')
- 
-root.geometry("500x400")
- 
-pygame.mixer.init()# initialise the pygame
- 
-def play():
-    pygame.mixer.music.load("ressources\\songs\\Avicii - The Nights.mp3")
-    pygame.mixer.music.play(loops=0)
- 
-title=Label(root,text="GeeksforGeeks",bd=9,relief=GROOVE,
-            font=("times new roman",50,"bold"),bg="white",fg="green")
-title.pack(side=TOP,fill=X)
- 
-play_button = Button(root, text="Play Song", font=("Helvetica", 32), command=play)
-play_button.pack(pady=20)
-root.mainloop()
-"""
 def main():
     
-    pygame.init()
-    window = tk.Tk()
+    window = Tk()
     pygame.mixer.init()
-    mixer.init()
     
     # Ajout d'un titre à la fenêtre principale :
     window.title("Spotizer")
     window.iconbitmap("ressources\\icons\\logo.ico")
-    window.anchor(tk.CENTER)
+    window.anchor(CENTER)
     window.geometry("600x750")
     window.resizable(False,False)
     window.config(bg='#5DC863')
@@ -54,27 +25,67 @@ def main():
     window.minsize(600,750)
 
     music_path = "ressources\\songs"
-    pattern = "*.mp3"
-
-    def play():
+    pattern = "*.mp3" 
+                        
+    def prev_music(): 
+        prev_song = music_space.curselection()
+        prev_song = prev_song[0]-1
+        prev_song_name = music_space.get(prev_song)
+        space.config(text= prev_song_name)
+        mixer.music.load(music_path + "\\" + prev_song_name + ".mp3")
+        mixer.music.play()
+        music_space.select_clear(0, 'end')
+        music_space.activate(prev_song)
+        music_space.select_set(prev_song)
+    
+    def stop_music():
+        mixer.music.stop()
+        music_space.select_clear('active')
+          
+    def play_music():
         space.config(text=music_space.get("anchor"))
-        #mixer.music.load("ressources\\songs\\Avicii - The Nights.mp3")
-        #vlc.MediaPlayer("ressources\\songs\\Avicii - The Nights.mp3").play()
-        #mixer.music.load(music_path + "\\" + music_space.get("anchor"))
-        #mixer.music.play()
+        #mixer.music.load(music_space.get(ACTIVE) + ".mp3")
+        mixer.music.load(music_path + "\\" + music_space.get("anchor") + ".mp3")
+        mixer.music.play()
         
-        path = music_path + "\\" + music_space.get("anchor") + ".mp3"
-        
-        playsound(path) 
-        
-        
-        #song = AudioSegment.from_mp3("ressources\\songs\\Avicii - The Nights.mp3")
-        #print('playing sound using  pydub')
-        #play(song)
 
+    def pause_music():
+        if(pause_button["text"]== "pause"):
+            mixer.music.pause()
+            pause_button["text"] = "play"
+        else:
+            mixer.music.unpause()
+            pause_button["text"]= "pause"
+        
+    def next_music():
+        next_song = music_space.curselection()
+        next_song = next_song[0]+1
+        next_song_name = music_space.get(next_song)
+        space.config(text= next_song_name)
+        mixer.music.load(music_path + "\\" + next_song_name + ".mp3")
+        mixer.music.play()
+        music_space.select_clear(0, 'end')
+        music_space.activate(next_song)
+        music_space.select_set(next_song)
+    
+    def import_music():
+        path_import = filedialog.askdirectory();
+        if path_import:
+            os.chdir(path_import)
+            songs=os.listdir(path_import)
+            for song in songs:
+                if song.endswith(".mp3"):
+                    music_space(END, song) 
+         
+    import_button = Button(window, text="Importer des musiques", bg="grey", fg="black", command=import_music)
+    import_button.pack(pady=5, padx=25, side=RIGHT)
  
-    music_space = tk.Listbox(window, fg="black", bg="grey", width=100,font=('helvetica',18))
+    music_space = Listbox(window, fg="black", bg="grey", width=100,font=('helvetica',18))
     music_space.pack(padx=18, pady=18)
+    
+    scroll_music = Scrollbar(window)
+    scroll_music.config(command=music_space.yview)
+    scroll_music.pack(side=RIGHT, fill=Y)
 
     for root, dirs, files, in os.walk(music_path):
         for filename in fnmatch.filter(files, pattern):
@@ -83,37 +94,37 @@ def main():
 
 
 
-    space = tk.Label(window, text="Boutons space",bg='#5DC863', fg='black', font=('poppin',22))
+    space = Label(window, text="Boutons space",bg='#5DC863', fg='black', font=('poppin',22))
     space.pack(pady=15)
     
-    buttons = tk.Frame(window, bg='grey')
+    buttons = Frame(window, bg='grey')
     buttons.pack(padx=10, pady=5,anchor='center') 
     
-    prev_icon = tk.PhotoImage(file="ressources\\icons\\prev.png")
-    prev_button = tk.Button(window, text="prev", image=prev_icon, bg='grey',borderwidth=0)
-    prev_button.pack(padx=8, pady=15, in_=buttons, side=tk.LEFT)
+    prev_icon = PhotoImage(file="ressources\\icons\\prev.png")
+    prev_button = Button(window, text="prev", image=prev_icon, bg='grey',borderwidth=0, command=prev_music)
+    prev_button.pack(padx=8, pady=15, in_=buttons, side=LEFT)
     
-    stop_icon = tk.PhotoImage(file="ressources\\icons\\stop.png")
-    stop_button = tk.Button(window, text="stop", image=stop_icon, bg='grey',borderwidth=0)
-    stop_button.pack(padx=8, pady=15, in_=buttons,side=tk.LEFT)
+    stop_icon = PhotoImage(file="ressources\\icons\\stop.png")
+    stop_button = Button(window, text="stop", image=stop_icon, bg='grey',borderwidth=0, command=stop_music)
+    stop_button.pack(padx=8, pady=15, in_=buttons,side=LEFT)
     
-    play_icon = tk.PhotoImage(file="ressources\\icons\\play.png")
-    play_button = tk.Button(window, text="play", image=play_icon, bg='grey',borderwidth=0, command=play)
-    play_button.pack(padx=8, pady=15, in_=buttons, side=tk.LEFT)
+    play_icon = PhotoImage(file="ressources\\icons\\play.png")
+    play_button = Button(window, text="play", image=play_icon, bg='grey',borderwidth=0, command=play_music)
+    play_button.pack(padx=8, pady=15, in_=buttons, side=LEFT)
     
-    pause_icon = tk.PhotoImage(file="ressources\\icons\\pause.png")
-    pause_button = tk.Button(window, text="pause", image=pause_icon, bg='grey',borderwidth=0)
-    pause_button.pack(padx=8, pady=15, in_=buttons,side=tk.LEFT)
+    pause_icon = PhotoImage(file="ressources\\icons\\pause.png")
+    pause_button = Button(window, text="pause", image=pause_icon, bg='grey',borderwidth=0, command=pause_music)
+    pause_button.pack(padx=8, pady=15, in_=buttons,side=LEFT)
     
-    next_icon = tk.PhotoImage(file="ressources\\icons\\next.png")
-    next_button = tk.Button(window, text="next", image=next_icon, bg='grey',borderwidth=0)
-    next_button.pack(padx=8, pady=15, in_=buttons, side=tk.LEFT)
+    next_icon = PhotoImage(file="ressources\\icons\\next.png")
+    next_button = Button(window, text="next", image=next_icon, bg='grey',borderwidth=0, command=next_music)
+    next_button.pack(padx=8, pady=15, in_=buttons, side=LEFT)
 
 
-    space2 = tk.Label(window, text="nav space",bg='#5DC863', fg='black', font=('grey',22))
+    space2 = Label(window, text="nav space",bg='#5DC863', fg='black', font=('grey',22))
     space2.pack(pady=15)
     
-    nav = tk.Frame(window, bg='white')
+    nav = Frame(window, bg='white')
     nav.pack(padx=10, pady=5,anchor='center')
     # Affichage de la fenêtre créée :
     window.mainloop()
@@ -123,4 +134,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-"""
