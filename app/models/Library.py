ce35dfa6da22ee8d6149214aaeb28879b55d4193
aoryuh <damien.coq2311@gmail.com>
2022-10-11 14:15:36 +0200

    basé sur branche de baptiste, update Music.py pour que la playlist se répète

--- app/models/Library.py
+++ app/models/Library.py
@@ -1,24 +1,29 @@
-#Modèle principal contenant les musiques et les playlists de l'app
+import os 
+import shutil
+import fnmatch
+import ctypes
+
+
+from tkinter import *
+from tkinter import filedialog
+import pygame
+from pygame import mixer
+
+#Classe permettant la gestion des musiques dans la librairie
 class Library :
-
-    #Composé de tous les éléments Music de l'app
-    music_list = []
-
-    #Composé d'éléments Tracks
-    playlists = []
-
-    def __init__(self, music_list, playlists):
-        self.music_list = music_list
-        self.playlists = playlists
-
-    def set_music_list(self, music_list):
-        self.music_list = music_list
-    
-    def set_playlists(self, playlists):
-        self.playlists = playlists
-    
-    def get_music_list(self):
-        return self.music_list
-    
-    def set_playlists(self):
-        return self.playlists
+    #Methode d'import de fichier mp3 
+    def import_music(music_path, music_space):
+        #Ouvrir la fenêtre de dialogue pour importer des fichier .mp3
+        songs=filedialog.askopenfilenames(initialdir="songs/",title="Importer fichier audio", filetypes=(("mp3 Files","*.mp3"),))
+        #Parcourir la selection
+        for song in songs: 
+            #Ne regarder que le nom de la musique
+            song_rename = song.replace("C:/Users/bapti/Music/","")
+            #Si elle a déjà été importé : Empêcher le re-import et afficher une popin
+            if(os.path.exists(music_path+"/"+ song_rename)):
+                ctypes.windll.user32.MessageBoxW(0, song_rename+" a déjà été importé", "Attention", 1)
+            #Si non, on l'ajoute à l'interface et dans le dossier
+            else:
+                shutil.copy(song, music_path)       
+                song = song.replace("C:/Users/bapti/Music/","")         
+                music_space.insert(END,song)
--- app/models/Music.py
+++ app/models/Music.py
@@ -1,22 +1,67 @@
-#Modèle représentant une musique et ses attributs
+from fileinput import filename
+from tkinter import *
+from app.models.Library import *
+
+from tkinter import filedialog
+import pygame
+from pygame import mixer
+
+#Classe permettant la gestion des musiques lors de la lecture
 class Music :
+    #Méthode permettant de jouer une musique
+    def play_music(music_path, music_space, space):
+        space.config(text=music_space.get("anchor"))
+        if(music_space.get("anchor") == ""):
+            music_space.select_set(0)
+            space.config(text=music_space.get(0))
+        mixer.music.load(music_path + "\\" + music_space.get("anchor") + ".mp3")
+        mixer.music.play()
 
-    title = ""
-
-    time_in_seconds = 0
-
-    path = ""
-
-    def __init__(self, title, time_in_seconds, path):
-        self.title = title
-        self.time_in_seconds = time_in_seconds
-        self.path = path
-
-    def get_title(self):
-        return self.title
+    #Méthode permettant de jouer la musique précédente
+    def prev_music(music_path, music_space, space): 
+        prev_song = music_space.curselection()
+        prev_song = prev_song[0]-1
+        prev_song_name = music_space.get(prev_song)
+        space.config(text= prev_song_name)
+        if (prev_song_name == ""):
+            music_space.select_set(music_space.size() - 1)
+            prev_song = music_space.size() - 1
+            prev_song_name = music_space.get(prev_song)
+            space.config(text=prev_song_name)
+        mixer.music.load(music_path + "\\" + prev_song_name + ".mp3")
+        mixer.music.play()
+        music_space.select_clear(0, 'end')
+        music_space.activate(prev_song)
+        music_space.select_set(prev_song)
     
-    def get_time_in_seconds(self):
-        return self.time_in_seconds
-
-    def get_path(self):
-        return self.path
+    #Méthode permettant de jouer la musique suivante
+    def next_music(music_path, music_space, space):
+        next_song = music_space.curselection()
+        next_song = next_song[0]+1
+        next_song_name = music_space.get(next_song)
+        space.config(text= next_song_name)
+        if (next_song_name == ""):
+            music_space.select_set(0)
+            next_song = 0
+            next_song_name = music_space.get(next_song)
+            space.config(text=next_song_name)
+        mixer.music.load(music_path + "\\" + next_song_name + ".mp3")
+        mixer.music.play()
+        music_space.select_clear(0, 'end')
+        music_space.activate(next_song)
+        music_space.select_set(next_song)
+    
+    #Méthode permettant de mettre en pause la musique courante
+    def pause_music(pause_button):
+        if(pause_button["text"]== "pause"):
+            mixer.music.pause()
+            pause_button["text"] = "play"
+        else:
+            mixer.music.unpause()
+            pause_button["text"]= "pause"
+            
+    #Méthode permettant d'arrêter la lecture d'une quelconque musique
+    def stop_music(music_space):
+        mixer.music.stop()
+        music_space.select_clear('active')
+          
